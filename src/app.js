f3584855a6d27fb456f46b8eb90c0500b49b19ca
Alberto Marín Wheelock <146878895+AlbertoMarinw@users.noreply.github.com>
2023-11-30 01:50:23 +0000

    generador de excusas de Alberto

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#EXCUSA").innerHTML = generarexcusa();
 };
+function generarexcusa() {
+  let pronombre = ["un", "el"];
+  let sujeto = ["perro", "gato", "vecino", "profesor"];
+  let verbo = ["comio", "destruyo", "mordio"];
+  let objeto = ["mi tarea", "mi proyecto", "mi lectura"];
+  let indicePronombre = Math.floor(Math.random() * pronombre.length);
+  let indiceSujeto = Math.floor(Math.random() * sujeto.length);
+  let indiceVerbo = Math.floor(Math.random() * verbo.length);
+  let indiceObjeto = Math.floor(Math.random() * objeto.length);
+  return (
+    pronombre[indicePronombre] +
+    " " +
+    sujeto[indiceSujeto] +
+    " " +
+    verbo[indiceVerbo] +
+    " " +
+    objeto[indiceObjeto]
+  );
+}
